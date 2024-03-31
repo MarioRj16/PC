23b96652a504a1c049e50ad0bbadd6c2a8d031da
rxfa <rafa3rnrn@gmail.com>
2024-03-31 00:36:42 +0000

    Optimized Exchanger

--- src/main/kotlin/pt/isel/pc/problemsets/set1/Exchanger.kt
+++ src/main/kotlin/pt/isel/pc/problemsets/set1/Exchanger.kt
@@ -1,44 +1,53 @@
 package pt.isel.pc.problemsets.set1
 
+import org.slf4j.LoggerFactory
 import java.util.concurrent.locks.ReentrantLock
 import java.util.concurrent.locks.Condition
 import kotlin.concurrent.withLock
 
 
 class Exchanger<T> {
+    private val logger = LoggerFactory.getLogger(Exchanger::class.java)
 
-    data class message<T>(var data:T, val locked:Condition)
     private val lock = ReentrantLock()
-    private val condition = lock.newCondition()
-    private var waiter: message<T>? = null
-    private var occupied:Boolean=false
 
-    fun exchange(myData: T): T? {
+    private data class Message<T>(
+        val condition: Condition,
+        var message: T,
+        var isDone: Boolean = false
+    )
+
+    private var sender: Message<T>? = null
+
+    private fun fastPath(data: T): T {
+        logger.debug("Node is going to fast path")
+        val node = sender!!
+        sender = null
+        val prevMessage = node.message
+        node.message = data
+        node.isDone = true
+        node.condition.signal()
+        logger.debug("Node received {} and sent {}", prevMessage, data)
+        return prevMessage
+    }
+
+    private fun waitPath(data: T): T{
+        logger.debug("Thread is going to wait-path")
+        sender = Message(lock.newCondition(), data)
+        val node = sender!!
+        while(true){
+            node.condition.await()
+            if(node.isDone){
+                logger.debug("Thread sent {} and received {}", data, node.message)
+                return node.message
+            }
+        }
+    }
+
+    fun exchange(data: T): T {
         lock.withLock {
-            while(occupied) condition.await()
-            var previous: T? = null
-            if (waiter == null) {
-                waiter = message(myData,lock.newCondition())
-                condition.signal()
-                waiter!!.locked.await()
-                    //wait for data update
-
-
-                previous = waiter!!.data
-                waiter = null
-                occupied=false
-                condition.signal()
-                } else {
-                    occupied=true
-                    previous = waiter!!.data
-
-                    waiter!!.data=myData
-                    waiter!!.locked.signal()
-
-                    //must signal so that first thread knows data has been changed
-
-                }
-                return previous
+            logger.debug("Thread wants to send {}", data)
+            return if (sender != null) fastPath(data) else waitPath(data)
         }
     }
 }
